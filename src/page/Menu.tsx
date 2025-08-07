bc12e3377206a5e54dbd5fd9c5bab6afec17c451
kevin170703 <correakevinfabian01@gmail.com>
2025-08-07 18:08:23 -0300

    fdgdf

--- src/page/Menu.tsx
+++ src/page/Menu.tsx
@@ -1,84 +1,100 @@
 "use client";
 
-import React, { useState } from "react";
+import { IconCurrencyDollar } from "@tabler/icons-react";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+
+interface Food {
+  name: string;
+  price: number;
+  main_image: string;
+  category: string;
+}
 
 export default function Menu() {
   const categories = ["Breakfast", "Lunch", "Dinner", "Dessert"];
 
-  const [categorySelected, setCategorySelected] = useState(categories[0]);
-
   const menu = [
     {
-      name: "comida 1",
+      name: "Huevos Rancheros",
       price: 12,
       main_image:
         "https://images.unsplash.com/photo-1482049016688-2d3e1b311543?q=80&w=710&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       category: "Lunch",
     },
     {
-      name: "comida 2",
+      name: "Asado Criollo",
       price: 9,
       main_image:
         "https://images.unsplash.com/photo-1555939594-58d7cb561ad1?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       category: "Dinner",
     },
     {
-      name: "comida 1",
+      name: "Tostadas con Palta",
       price: 12,
       main_image:
         "https://images.unsplash.com/photo-1482049016688-2d3e1b311543?q=80&w=710&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       category: "Breakfast",
     },
     {
-      name: "comida 2",
+      name: "Cheesecake de Frutilla",
       price: 9,
       main_image:
         "https://images.unsplash.com/photo-1555939594-58d7cb561ad1?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       category: "Dessert",
     },
     {
-      name: "comida 1",
+      name: "Sándwich de Milanesa",
       price: 12,
       main_image:
         "https://images.unsplash.com/photo-1482049016688-2d3e1b311543?q=80&w=710&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       category: "Lunch",
     },
     {
-      name: "comida 2",
+      name: "Pasta Bolognesa",
       price: 9,
       main_image:
         "https://images.unsplash.com/photo-1555939594-58d7cb561ad1?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       category: "Dinner",
     },
     {
-      name: "comida 1",
+      name: "Brownie con Helado",
       price: 12,
       main_image:
         "https://images.unsplash.com/photo-1482049016688-2d3e1b311543?q=80&w=710&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       category: "Dessert",
     },
     {
-      name: "comida 2",
+      name: "Medialunas y Café",
       price: 9,
       main_image:
         "https://images.unsplash.com/photo-1555939594-58d7cb561ad1?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       category: "Breakfast",
     },
     {
-      name: "comida 1",
+      name: "Bowl de Quinoa",
       price: 12,
       main_image:
         "https://images.unsplash.com/photo-1482049016688-2d3e1b311543?q=80&w=710&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       category: "Lunch",
     },
     {
-      name: "comida 2",
+      name: "Pizza Margarita",
       price: 9,
       main_image:
         "https://images.unsplash.com/photo-1555939594-58d7cb561ad1?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       category: "Dinner",
     },
   ];
+
+  const [categorySelected, setCategorySelected] = useState(categories[0]);
+
+  const [menuSelectd, setMenuSelected] = useState<Food[] | null>(null);
+
+  useEffect(() => {
+    const foods = menu.filter((food) => food.category === categorySelected);
+    setMenuSelected(foods);
+  }, [categorySelected]);
 
   return (
     <main>
@@ -101,7 +117,30 @@ export default function Menu() {
         </div>
       </section>
 
-      <section></section>
+      <section className="relative flex flex-wrap justify-center items-center gap-2 py-5">
+        {menuSelectd &&
+          menuSelectd.map((food) => (
+            <div key={food.name} className="relative w-[460px] aspect-square">
+              <Image
+                src={food.main_image}
+                alt={`food ${food.name}`}
+                width={500}
+                height={500}
+                className="object-cover absolute top-0 left-0 w-full h-full"
+              />
+
+              <div className="relative z-10 w-full h-full flex flex-col justify-end items-center bg-gradient-to-t from-primary  to-transparent to-40% p-4 gap-2">
+                <p className="text-white text-3xl">{food.name}</p>
+                <div className="flex justify-center items-center gap-8">
+                  <div className="flex justify-center items-center  text-white">
+                    <IconCurrencyDollar className="size-6" />
+                    <p className="text-xl">{food.price}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+      </section>
     </main>
   );
 }
